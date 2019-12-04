75a2ede74c1b713b61c5f18516ff38b1bc0f5595
Lauren Girolmetti <laurengiro@gmail.com>
2019-12-03 20:25:17 -0500

    Finished solution

---
+++ LiveCoding3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiveCoding3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 1, 5, 2 };
+            int[] array2 = new int[] { 3, 4, 6 };
+            ZippedArray(array1, array2);
+        }
+
+        static void ZippedArray(int[] array1, int[] array2)
+        {
+            int zipped_len = 2 * array1.Length;
+            int[] zippedArray = new int[zipped_len];
+
+            int single_array_position = 0;
+
+            for (int x = 0; x < array1.Length; x++)
+            {
+                zippedArray[single_array_position] = array1[x];
+                single_array_position++;
+                zippedArray[single_array_position] = array2[x];
+                single_array_position++;
+            }
+
+            Console.WriteLine("[" + string.Join(",", zippedArray) + "]");
+        }
+    }
+}
